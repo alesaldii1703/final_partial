231fe29502723f553df6ffdf8da689dae0a754d4
alesaldii1703 <145026000+alesaldii1703@users.noreply.github.com>
2023-11-05 20:22:14 -0600

    Update example_code_02.py

--- example_code_02.py
+++ example_code_02.py
@@ -1,1 +1,10 @@
-
+#this an example of a code using conditionals
+#Invitation link: https://replit.com/join/dyonqvxxvd-alejandrasaldiv 
+print("Hello, friend!")
+age = int(input("How old are you?"))
+if age >= 18 and age <= 25:
+  print("You are a small adult. Nice!")
+elif age > 25:
+  print("You are an adult. Wow.")
+else:
+  print("You are just a kid! Awesome!")
