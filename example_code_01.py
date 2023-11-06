a5565f9f03c390e4c816714d4ea936b39f827b4c
alesaldii1703 <145026000+alesaldii1703@users.noreply.github.com>
2023-11-05 20:07:43 -0600

    Create example_code_01.py

---
+++ example_code_01.py
@@ -0,0 +1,18 @@
+#Here is a code example just for you to know how the code should look
+#Invitation link: https://replit.com/join/sbyszkurwq-alejandrasaldiv 
+print("Hello, Human!")
+name = input("What is your name? ")
+print(f"""
+
+Nice to meet you, {name}.
+
+Today is a great day to be coding.
+
+Take some tea, open your replit and start creating.
+
+It is nice you are here reading my code.
+
+Have an awesome week! 
+
+And remember: don't worry be happy.
+""")
